e5d81533bb125a131ad71d15c9f0e30f2919231c
timothy-park <timothypark713@gmail.com>
2016-10-13 20:18:12 -0700

    added the layout for regression functions.R

---
+++ code/functions/regression-functions.R
@@ -0,0 +1,19 @@
+residual_sum_squares <- function(object) {
+  
+}
+
+total_sum_squares <- function(object) {
+  
+}
+
+r_squared <- function(object) {
+  
+}
+
+f_statistic <- function(object) {
+  
+}
+
+residual_std_error <- function(objects) {
+  
+}
