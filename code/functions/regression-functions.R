c94a325cb7a6e4051169e36c69f2ee647209aaa4
timothy-park <timothypark713@gmail.com>
2016-10-13 21:07:28 -0700

    made test-that.R and completed regression functions

--- code/functions/regression-functions.R
+++ code/functions/regression-functions.R
@@ -1,19 +1,26 @@
 residual_sum_squares <- function(object) {
-  
+  rss <- sum(object$residuals)
+  return(rss)
 }
 
 total_sum_squares <- function(object) {
-  
+  sum <- summary(object)
+  tss <- sum(sum$residuals)/(1-sum$r.squared)
 }
 
 r_squared <- function(object) {
-  
+  sum <- summary(object)
+  r_s <- sum$r.squared
 }
 
 f_statistic <- function(object) {
-  
+  sum <- summary(object)
+  f_stat <- sum$fstatistic
+  return(f_stat)
 }
 
 residual_std_error <- function(objects) {
-  
+  sum <- summary(object)
+  rse <- sqrt(sum$sigma)
+  return(rse)
 }
---
+++ code/test-that.R
@@ -0,0 +1,12 @@
+source('../functions.R')
+
+context('Testing for regression statistics on mtcars dataset')
+test_that('Regression outputs work as expected', {
+  reg <- lm(mpg ~ disp + hp, data = mtcars)
+  regsum <- summary(reg)
+  expect_equal(residual_sum_squares(reg), reg$residuals^2)
+  expect_equal(total_sum_squares(reg), sum((mtcars$mpg - mean(mtcars$mpg))^2))
+  expect_equal(residual_std_error(reg), regsum$sigma)
+  expect_equal(r_squared(reg), regsum$r.squared)
+  expect_equal(f_statistic(reg), regsum$fstatistic[1])
+})
