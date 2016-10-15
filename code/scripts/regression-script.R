20d4afdffd533ff2a5d7c09deeb6ae663d1a17b6
timothy-park <timothypark713@gmail.com>
2016-10-14 23:57:59 -0700

    finishing touches to plots and rmd file typos

--- code/scripts/regression-script.R
+++ code/scripts/regression-script.R
@@ -7,16 +7,24 @@ head(ad)
 tv.fit = lm(Sales ~ TV, data = ad)
 png('images/scatterplot-tv-sales.png')
 plot(TV, Sales, main = 'Sales vs. TV')
+abline(tv.fit, lwd = 3, col = 'red')
+line <- tv.fit[[1]][2]*TV + tv.fit[[1]][1]
+for (i in 1:200) {
+  segments(TV[i], Sales[i],
+           TV[i], line[i], lwd = 1)
+}
 dev.off()
 
 radio.fit <- lm(Sales ~ Radio, data = ad)
 png('images/scatterplot-radio-sales.png')
 plot(Radio, Sales, main = 'Sales vs. Radio')
+abline(radio.fit, lwd = 3, col = 'red')
 dev.off()
 
 news.fit <- lm(Sales ~ Newspaper, data = ad)
 png('images/scatterplot-newspaper-sales.png')
 plot(Newspaper, Sales, main = 'Sales vs. Newspaper')
+abline(news.fit, lwd = 3, col = 'red')
 dev.off()
 
 lm.fit <- lm(Sales ~ TV + Radio + Newspaper, data = ad)
