78254aa80c48cf6afd792372d18a38cc1d545ddb
timothy-park <timothypark713@gmail.com>
2016-10-14 22:50:22 -0700

    updated the function description in regression-function.R

--- code/functions/regression-functions.R
+++ code/functions/regression-functions.R
@@ -1,9 +1,12 @@
+# function that takes in the the lm object and outputs the residual sum of squares of the fit
+
 residual_sum_squares <- function(object) {
   residuals <- object$residuals
   rss <- sum((residuals)^2)
   return(rss)
 }
 
+# function that takes in the lm object and outputs the total sum of squares of the fit
 total_sum_squares <- function(object) {
   mean_y <- mean(object$model[,1])
   y <- object$model[,1]
@@ -11,6 +14,8 @@ total_sum_squares <- function(object) {
   return(tss)
 }
 
+# functon that takes in the lm object and outputs its residual standard error
+# calls the function residual_sum_squares
 residual_std_error <- function(object) {
   p <- length(object$coefficients[-1])
   n <- nrow(object$model)
@@ -19,6 +24,7 @@ residual_std_error <- function(object) {
   return(rse)
 }
 
+# function that takes in the lm object and returns the r_square quality index of the fit
 r_squared <- function(object) {
   rss <- residual_sum_squares(object)
   tss <- total_sum_squares(object)
@@ -26,6 +32,7 @@ r_squared <- function(object) {
   return(r_sq)
 }
 
+# function that takes in the lm object and returns the f-statistic of the fit
 f_statistic <- function(object) {
   p <- length(object$coefficients[-1])
   n <- nrow(object$model)
